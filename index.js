949f48bdd2fb81f505a0bcaaabbfa5de4a6ab03b
Dat Ngo <datngonyc@gmail.com>
2023-04-30 19:48:41 -0400

    Added the skeleton for the three main containers (top, middle, and bottom)

---
+++ index.js
@@ -0,0 +1,8 @@
+/* Instantiate element references */
+// we need logic behind the clear all button
+// We need logic behind button/input/whatever we'll use to specify number of cells to create
+// we need logic to create the cells themselves
+const clear = document.querySelector('#clear');
+const slider = document.querySelector('#number-of-cells');
+const canvas = document.querySelector('#canvas');
+
