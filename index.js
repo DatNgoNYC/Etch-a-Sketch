c1018ad1abbc80222b5bf6e4f2ff441506148c8a
Dat Ngo <datngonyc@gmail.com>
2023-04-30 22:23:22 -0400

    Added a function to draw the canvas cells

--- index.js
+++ index.js
@@ -1,8 +1,40 @@
-/* Instantiate element references */
-// we need logic behind the clear all button
-// We need logic behind button/input/whatever we'll use to specify number of cells to create
-// we need logic to create the cells themselves
-const clear = document.querySelector('#clear');
-const slider = document.querySelector('#number-of-cells');
-const canvas = document.querySelector('#canvas');
+drawCanvas(10);
+/* Logic for the input form */
+// Instantiate submitButton and add an event listener
+const submitButton = document.querySelector('#canvas-size-form button');
+submitButton.addEventListener('click', changeCanvasSize);
 
+// Get the value of the input using the onClicked button
+function changeCanvasSize() {
+    inputValue = submitButton.previousElementSibling.value;
+    drawCanvas(inputValue);
+};
+
+// Draws the canvas with the width parameter serving as both width and height
+function drawCanvas(width) {
+    // create a variable that is a div which will contain all the canvas cells.
+    const temporaryInnerCanvas = document.createElement('div');
+    temporaryInnerCanvas.setAttribute('id', 'inner-canvas');
+
+    // draw the canvas. Populate the temporary innerCanvas variable above.
+    // first for loop will create the rows
+    for (let index = 0; index < width; index++) {
+        const canvasRow = document.createElement('div');
+        canvasRow.setAttribute('class', 'canvas-row');
+        
+        // create the cells inside the rows
+        for (let index = 0; index < width; index++) {
+            const canvasCell = document.createElement('div');
+            canvasCell.setAttribute('class', 'canvas-cell');
+            
+            canvasRow.append(canvasCell);
+        }
+
+        temporaryInnerCanvas.append(canvasRow);
+    }
+
+    const innerCanvas = document.querySelector('#inner-canvas');
+    innerCanvas.remove();
+    const canvas = document.querySelector('#canvas');
+    canvas.appendChild(temporaryInnerCanvas);
+}
