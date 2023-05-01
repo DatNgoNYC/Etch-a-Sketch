07e8accc418adb292ac28812f2d316f5717177ca
Dat Ngo <datngonyc@gmail.com>
2023-05-01 10:35:15 -0400

    Add painting functionality

--- index.js
+++ index.js
@@ -1,40 +1,60 @@
+/*  --- Initial State --- */
+/*  --- DESCRIPTION: the initial state of etch-a-sketch is a 10x10 canvas */
 drawCanvas(10);
-/* Logic for the input form */
-// Instantiate submitButton and add an event listener
-const submitButton = document.querySelector('#canvas-size-form button');
-submitButton.addEventListener('click', changeCanvasSize);
 
-// Get the value of the input using the onClicked button
+/*  --- Logic for the input form --- */
+/*  --- DESCRIPTION: THe input form (input and button) will change the canvas dimesions */
+// Instantiate reference to the submitButton and add an event listener that will change the canvas dimension based on the input value;
+const submitButton = document.querySelector("#canvas-size-form button");
+submitButton.addEventListener("click", changeCanvasSize);
+
+// Change canvas dimensions based on value of input element
 function changeCanvasSize() {
-    inputValue = submitButton.previousElementSibling.value;
-    drawCanvas(inputValue);
-};
+	inputValue = submitButton.previousElementSibling.value;
+	if (Object.is(parseInt(inputValue), NaN)) {
+		// don't do anything if the input value is empty
+		return;
+	} else {
+		drawCanvas(parseInt(inputValue));
+	}
+}
 
 // Draws the canvas with the width parameter serving as both width and height
 function drawCanvas(width) {
-    // create a variable that is a div which will contain all the canvas cells.
-    const temporaryInnerCanvas = document.createElement('div');
-    temporaryInnerCanvas.setAttribute('id', 'inner-canvas');
+	const temporaryInnerCanvas = document.createElement("div"); // div which will contain all the canvas cells.
+	temporaryInnerCanvas.setAttribute("id", "inner-canvas");
+	console.log(typeof width);
+	console.log(width);
 
-    // draw the canvas. Populate the temporary innerCanvas variable above.
-    // first for loop will create the rows
-    for (let index = 0; index < width; index++) {
-        const canvasRow = document.createElement('div');
-        canvasRow.setAttribute('class', 'canvas-row');
-        
-        // create the cells inside the rows
-        for (let index = 0; index < width; index++) {
-            const canvasCell = document.createElement('div');
-            canvasCell.setAttribute('class', 'canvas-cell');
-            
-            canvasRow.append(canvasCell);
-        }
+	for (let index = 0; index < width; index++) {
+		const canvasRow = document.createElement("div"); // Populate the temporaryInnerCanvas instantiated above
+		canvasRow.setAttribute("class", "canvas-row");
 
-        temporaryInnerCanvas.append(canvasRow);
-    }
+		for (let index = 0; index < width; index++) {
+			const canvasCell = document.createElement("div");
+			canvasCell.setAttribute("class", "canvas-cell");
+			canvasCell.addEventListener('mouseover', coloredIn); // Add an event listener to these cells
 
-    const innerCanvas = document.querySelector('#inner-canvas');
-    innerCanvas.remove();
-    const canvas = document.querySelector('#canvas');
-    canvas.appendChild(temporaryInnerCanvas);
+			canvasRow.append(canvasCell);
+		}
+
+		temporaryInnerCanvas.append(canvasRow);
+	}
+
+	const innerCanvas = document.querySelector("#inner-canvas");
+	innerCanvas.remove();
+	const canvas = document.querySelector("#canvas");
+	canvas.appendChild(temporaryInnerCanvas);
 }
+
+/*  ---- Painting Canvas Logic ---- */
+/*  ---- DESCRIPTION: Canvas cells will be colored in when hovered over */
+// Create a function that toggles on the .colored class
+function coloredIn(event) {
+	event.target.classList.add('coloredIn');
+}
+
+/*  ---- Clear button logic --- */
+/*  ---- DESCRIPTION: Clear button will return the canvas to a blank state */
+// Reference to clear button
+// Add an event listener for when the button is clicked, will call clear canvas on event action. Clear canvas won't delete the div but change it's color to white/
